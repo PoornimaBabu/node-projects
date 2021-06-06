81b730c47c4b3879108c4ba99ab1857b34d76f6e
BABU Poornima <poornima.babu@soprasteria.com>
2021-06-06 18:05:43 +0530

    Completed upto styling the page.

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -1,15 +1,18 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 //Defining path for static and views
 const publicPath = path.join(__dirname,'../public')
 const viewPath = path.join(__dirname,'../templates/views')
+const partialPath = path.join(__dirname, '../templates/partials')
 
 //Serving Views and View Engine
 app.set('views',viewPath)
 app.set('view engine', 'hbs')
+hbs.registerPartials(partialPath)
 
 //Serving static files
 app.use(express.static(publicPath))
@@ -17,21 +20,41 @@ app.use(express.static(publicPath))
 
 app.get('', (req,res) => {
     res.render('index', {
-        title: 'Weather App'
+        title: 'Weather App',
+        name: 'Poornima Babu.'
     })
 })
 
 app.get('/about', (req,res) => {
     res.render('about', {
-        title: 'Created by Poornima.'
+        name: 'Poornima Babu.',
+        title: 'About'
     })
 })
 
 app.get('/help', (req,res) => {
     res.render('help', {
-        title: 'For help contact Poornima..'
+        title: 'Help',
+        name: 'Poornima Babu.'
     })
 })
+
+app.get('/help/*', (req,res) => {
+    res.render('404help', {
+        name: 'Poornima Babu.',
+        title: '404',
+        errormessage: 'Help article not found!'
+    })
+})
+
+app.get('*', (req,res) => {
+    res.render('404page', {
+        name: 'Poornima Babu.',
+        title: '404',
+        errormessage: 'Page not found!'
+    })
+})
+
 
 app.listen(3000, () => {
     console.log('Server running on port 3000.')
